2dea8d709c1000c7ffeef784869721e71a20939c
Matheus Tavares <matheustavares1a@gmail.com>
2025-12-01 21:06:43 -0300

    snapshot test

--- src/componentes/Menu/Menu.test.jsx
+++ src/componentes/Menu/Menu.test.jsx
@@ -17,4 +17,11 @@ test("It shan't render link to extract", () => {
     render(<Menu />)
     const extractLink = screen.queryByText('extrato');
     expect(extractLink).not.toBeInTheDocument();
+});
+
+test('It shall render a list of links with className link', () => {
+    render(<Menu />)
+    const links = screen.getAllByRole('link')
+    links.forEach((link) => expect(link).toHaveClass('link'))
+    expect(links).toMatchSnapshot()
 })
