a3b655e52b854f09ec86dff6386257630f9346d0
Matheus Tavares <matheustavares1a@gmail.com>
2025-12-02 10:47:43 -0300

    Testing a function

--- src/utils/Utils.test.js
+++ src/utils/Utils.test.js
@@ -1,3 +1,27 @@
+import { calculaNovoSaldo } from './index';
+
+describe('When I make a transaction', () => {
+    test("Since it's a deposit, the balance should increase.", () => {
+        const transaction = {
+            transacao: 'Depósito',
+            valor: 100,
+        };
+
+        const newBalance = calculaNovoSaldo(transaction, 0);
+        expect(newBalance).toBe(100);
+    });
+
+    test("Since it's a transfer, the balance should decrease.", () => {
+        const transaction = {
+            transacao: 'Transferência',
+            valor: 100,
+        };
+
+        const newBalance = calculaNovoSaldo(transaction, 300);
+        expect(newBalance).toBe(200);
+    });
+});
+
 test('It should render the updated balance value with the accrued interest.', () => {
     const calculatePerformance = jest.fn(
         (performance) => performance + performance * 0.005,
